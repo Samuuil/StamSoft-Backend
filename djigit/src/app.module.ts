c95a04d584340d168a9ce57720ebb710ab82a028
Samuuil <atanas872@gmail.com>
2025-07-01 15:13:05 +0300

    added database setup

--- djigit/src/app.module.ts
+++ djigit/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT!, 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+        autoLoadEntities: true,
+        synchronize: true, 
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- djigit/typeorm/src/data-source.ts
+++ djigit/typeorm/src/data-source.ts
@@ -1,17 +1,22 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from './entity/User';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME || 'test',
+  password: process.env.DB_PASSWORD || 'test',
+  database: process.env.DB_NAME || 'test',
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: ['./migrations/*.ts'],
+  subscribers: [],
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+});
+
