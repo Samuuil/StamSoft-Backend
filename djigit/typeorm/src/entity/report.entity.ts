3b250ab8865801228847ce061083d9f0cc2cf5db
Ilina Tsanova <107075825+Ilinats@users.noreply.github.com>
2025-07-02 15:53:26 +0300

    report entity

--- djigit/typeorm/src/entity/report.entity.ts
+++ djigit/typeorm/src/entity/report.entity.ts
@@ -7,8 +7,27 @@ export class Report {
   id: number;
 
   @Column()
+  licensePlate: string;
+
+  @Column()
   description: string;
+
+  @Column('double precision')
+  latitude: number;
+
+  @Column('double precision')
+  longitude: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  imageUrl: string;
+
+  @Column({ nullable: true })
+  videoUrl?: string;
 
   @ManyToOne(() => User, (user) => user.reports, { onDelete: 'SET NULL' })
   reportedBy: User;
 }
+
