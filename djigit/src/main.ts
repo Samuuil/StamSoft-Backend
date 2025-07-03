a39c6f4cb6ad0ee4585e9db2f7b52523f1ac50f2
Ilina Tsanova <107075825+Ilinats@users.noreply.github.com>
2025-07-03 23:24:51 +0300

    cors setup

--- djigit/src/main.ts
+++ djigit/src/main.ts
@@ -6,6 +6,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    });
+
     const config = new DocumentBuilder()
     .setTitle('StamSoft API')
     .setDescription('API documentation for StamSoft Backend')
