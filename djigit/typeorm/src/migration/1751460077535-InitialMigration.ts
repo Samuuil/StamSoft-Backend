93aa68ddf9287dd508ddf6e34d42c684607464ec
Samuuil <atanas872@gmail.com>
2025-07-03 14:21:04 +0300

    fixed db issues

---
+++ djigit/typeorm/src/migration/1751460077535-InitialMigration.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1751460077535 implements MigrationInterface {
+    name = 'InitialMigration1751460077535'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "report" ADD "licensePlate" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "report" ADD "latitude" double precision NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "report" ADD "longitude" double precision NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "report" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "report" ADD "imageUrl" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "report" ADD "videoUrl" character varying`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // await queryRunner.query(`ALTER TABLE "report" DROP COLUMN "videoUrl"`);
+        // await queryRunner.query(`ALTER TABLE "report" DROP COLUMN "imageUrl"`);
+        // await queryRunner.query(`ALTER TABLE "report" DROP COLUMN "createdAt"`);
+        // await queryRunner.query(`ALTER TABLE "report" DROP COLUMN "longitude"`);
+        // await queryRunner.query(`ALTER TABLE "report" DROP COLUMN "latitude"`);
+        // await queryRunner.query(`ALTER TABLE "report" DROP COLUMN "licensePlate"`);
+    }
+
+}
